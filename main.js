8bf1805dd6b277ea81f983312b2a4ac9b69a3073
handacg <handa.cheng@gmail.com>
2018-09-20 19:55:01 +0200

    UPDATE

---
+++ main.js
@@ -0,0 +1,17 @@
+window.onload = setTimeout(function(){
+if (confirm('Tu veux venir sur xXxvidsxXx ?')) {
+	window.location = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+}
+else {
+	return false ;
+}
+}, 10000);
+
+
+
+
+//background turns to magenta.
+//all para turn font to blue
+//all headings font turn to green 
+//all paras font turn to papyrus and headers font to  Comic sans 
+
